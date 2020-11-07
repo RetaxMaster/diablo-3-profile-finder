61143e9d5067a16803d255236a20e13aca31dcec
RetaxMaster <retaxmaster@hotmail.com>
2020-11-06 23:30:15 -0600

    Refactorizacion del codigo para separar el importado de librerias

--- src/main.js
+++ src/main.js
@@ -1,40 +1,19 @@
 import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-// Iconos de tipo "Solid"
-import { faSkull, faCrown, faDungeon, faHatWizard, faHammer, faGem } from '@fortawesome/free-solid-svg-icons'
-// Iconos de tipo "Brand" (marcas o logos de empresas)
-import { faVuejs, faBootstrap, faFontAwesome, faGithub, faBattleNet } from '@fortawesome/free-brands-svg-icons'
-// El componente que vamos a utilizar
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+// BootstrapVue
+import './plugins/bootstrapVue'
+
+// Vue Font-Awesome
+import './plugins/fontAwesome'
 
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+// CSS General
 import './assets/css/main.styl'
 
-// Le añadimos los iconos que acabamos de importar (todos, los de tipo solid y de tipo brand)
-library.add(
-  faSkull,
-  faCrown,
-  faDungeon,
-  faHatWizard,
-  faHammer,
-  faGem,
-  faVuejs,
-  faBootstrap,
-  faFontAwesome,
-  faGithub,
-  faBattleNet
-)
-
-Vue.use(BootstrapVue)
 Vue.config.productionTip = false
-Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
 
