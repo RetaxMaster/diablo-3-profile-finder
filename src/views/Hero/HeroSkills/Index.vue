720af9b8c1348f951e4f137217be64cbbf57a0bb
RetaxMaster <retaxmaster@hotmail.com>
2020-11-08 02:48:10 -0600

    Refactorizacion del codigo para usar lazy loading de componentes y keep alive

--- src/views/Hero/HeroSkills/Index.vue
+++ src/views/Hero/HeroSkills/Index.vue
@@ -3,24 +3,41 @@
     <h2 class="font-diablo">Skills</h2>
     <hr class="bg-white">
 
-    <ActiveSkills :skills="skills.active"/>
+    <b-nav pills small>
+      <b-nav-item :active="!isPassiveSkillsActive" @click="changeComponent('ActiveSkills')">Active</b-nav-item>
+      <b-nav-item :active="isPassiveSkillsActive" @click="changeComponent('PassiveSkills')">Passive</b-nav-item>
+    </b-nav>
+
+    <!-- Con keep-alive logramos que el componente no se vuelva a re-renderizar una vez que ya estaba renderizado -->
+    <keep-alive>
+      <!-- component es un componente que me permite cargar cualquier componente a través de su propiedad is, es bueno para usar lazy loading de componentes -->
+      <component :is="activeComponent" :skills="componentProps"/>
+    </keep-alive>
+
+    <!-- <ActiveSkills :skills="skills.active"/>
     <hr>
-    <PassiveSkills :skills="skills.passive"/>
+    <PassiveSkills :skills="skills.passive"/> -->
 
   </div>
 </template>
 
 <script>
-import ActiveSkills from './ActiveSkills'
-import PassiveSkills from './PassiveSkills'
+/* import ActiveSkills from './ActiveSkills'
+import PassiveSkills from './PassiveSkills' */
 
 export default {
 
   name: 'HeroSkills',
 
+  data () {
+    return {
+      activeComponent: 'ActiveSkills'
+    }
+  },
+
   components: {
-    ActiveSkills,
-    PassiveSkills
+    ActiveSkills: () => import(/* webpackChunkName: "ActiveSkills" */'./ActiveSkills'),
+    PassiveSkills: () => import(/* webpackChunkName: "PassiveSkills" */'./PassiveSkills')
   },
 
   props: {
@@ -28,6 +45,33 @@ export default {
       required: true,
       type: Object
     }
+  },
+
+  methods: {
+
+    changeComponent (component) {
+      this.activeComponent = component
+    }
+
+  },
+
+  computed: {
+
+    /**
+     * Dinamyc props for async dynamic components
+     * @returns {String}
+     */
+    // Con esto estamos generando "props" dinámicas
+    // Si el componente es ActiveSkills pasa como props las activas, si no, las pasivas
+    componentProps () {
+      return this.activeComponent === 'ActiveSkills' ? this.skills.active : this.skills.passive
+    },
+
+    // Nos dice si el componente "HabilidadesPasivas" está activo o no
+    isPassiveSkillsActive () {
+      return this.activeComponent === 'PassiveSkills'
+    }
+
   }
 
 }
